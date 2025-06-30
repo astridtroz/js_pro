d0327a2b1de4dfc1d0f0114e1aff5ac198431a46
Saumya Srivastava <112683495+astridtroz@users.noreply.github.com>
2025-06-30 11:43:42 +0000

    conversion of datatypes

---
+++ basics/conversion.js
@@ -0,0 +1,24 @@
+let score="33abs"
+console.log(typeof score);
+
+let valuInNumber= Number(score)
+console.log(typeof valuInNumber);
+console.log(valuInNumber);
+
+/*"33"=> 33
+"33abs"=>NaN
+true=>1; false=>0
+*/
+let isLogggedIn=""
+let booleanIsLoggedIn= Boolean(isLogggedIn);
+console.log( booleanIsLoggedIn);
+
+/*
+1=>true; 0=> false
+""=> false
+"hitesh"=> true
+ */
+
+let someNumber=33
+let stringNumber= String(someNumber);
+console.log(typeof stringNumber);
