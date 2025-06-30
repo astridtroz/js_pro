5b483167af70c9d84df2e3ecffbf63a9fc3dd73e
Saumya Srivastava <112683495+astridtroz@users.noreply.github.com>
2025-06-30 12:16:18 +0000

    conversion

--- basics/conversion.js
+++ basics/conversion.js
@@ -1,9 +1,9 @@
 let score="33abs"
-console.log(typeof score);
+//console.log(typeof score);
 
 let valuInNumber= Number(score)
-console.log(typeof valuInNumber);
-console.log(valuInNumber);
+//console.log(typeof valuInNumber);
+//console.log(valuInNumber);
 
 /*"33"=> 33
 "33abs"=>NaN
@@ -11,7 +11,7 @@ true=>1; false=>0
 */
 let isLogggedIn=""
 let booleanIsLoggedIn= Boolean(isLogggedIn);
-console.log( booleanIsLoggedIn);
+//console.log( booleanIsLoggedIn);
 
 /*
 1=>true; 0=> false
@@ -21,4 +21,22 @@ console.log( booleanIsLoggedIn);
 
 let someNumber=33
 let stringNumber= String(someNumber);
-console.log(typeof stringNumber);
+//console.log(typeof stringNumber);
+
+/*
+
++, -, *, **, /, %
+
+*/
+
+let str1="hello";
+let str2="saumya";
+let str3= str1+str2;
+console.log(str3)
+
+console.log("1"+2);
+console.log(1+"2");
+console.log("1"+ 2+ 2); //=>122
+console.log(1+2+"2");//=>32
+
+
