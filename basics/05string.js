b50f12bf449fb43a9a4918cb37becd30524b993c
Saumya Srivastava <112683495+astridtroz@users.noreply.github.com>
2025-06-30 14:12:59 +0000

    strings

---
+++ basics/05string.js
@@ -0,0 +1,26 @@
+let name="Saumya"
+let repoCount="50"
+
+console.log(`my name is ${name} and my repo count is ${repoCount}`);
+
+const gameName= new String('hitesh')
+
+console.log(gameName[0])
+console.log(gameName.__proto__);
+
+console.log(gameName.length);
+console.log(gameName.charAt(2));
+console.log(gameName.indexOf('t'));
+
+const newString= gameName.substring(0,4);
+
+const anotherString= gameName.slice(-6,4)
+console.log(anotherString)
+
+const dtr="  to  "
+
+console.log(dtr.replace('to', '-'))
+console.log(dtr.trim())
+
+console.log(dtr.includes('my'))
+console.log(gameName.split("t"))
