3d74429e49dd71d6483cc9bf9bbcd4fd96db716b
Saumya Srivastava <112683495+astridtroz@users.noreply.github.com>
2025-07-07 06:27:48 +0000

    scope

---
+++ 03_basics/02_scope.js
@@ -0,0 +1,37 @@
+let a=300
+var c=300
+if(true){
+    let a=10
+    const b=20
+    var c=30
+    // console.log(`block scope value of a=${a}`)
+    // console.log(`block scope value of c=${c}`)
+}
+
+// console.log(`global scope value of a=${a}`)
+// console.log(`global scope value of c changes due to declaration by var =${c}`)
+
+function outer(){
+    username="saumya"
+    function inner(){
+        website="web"
+        console.log("name")
+    }
+    //console.log(website)//not available outside scope of inner
+    inner()//executed only when called
+}
+
+//outer()
+//++++++++++++++++++++++++interesting+++++++++++++++++++++++
+
+console.log(addone(5)) //can access addone before declaration
+function addone(num){
+    return num+1
+}
+
+//console.log(result) //cannot access addTwo due to holding by a variable
+const result= function addtwo(num){
+    return num+2
+}
+
+
