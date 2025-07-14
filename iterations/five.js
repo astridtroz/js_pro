eebaf829d7a1ed39cb67ba918cacc64d6abceefd
Saumya Srivastava <112683495+astridtroz@users.noreply.github.com>
2025-07-14 15:50:02 +0000

    five

--- iterations/five.js
+++ iterations/five.js
@@ -9,7 +9,30 @@ const coding=["js","cpp","swift","python","ruby"]
 //passing reference of a function to for each
 
 function printme(item){
-     console.log(item)
+     //console.log(item)
 }
 
-coding.forEach(printme)
+//coding.forEach(printme)
+
+coding.forEach((item, index, arr)=>{
+    //console.log(item, index, arr)
+})
+
+//array of object
+
+const myCoding=[
+    { lname: "js",
+      lf: "j"
+    },
+    { 
+        lname: "python",
+      lf: "py"
+    },
+    {
+        lname: "c++",
+      lf: "cpp"
+    },
+]
+myCoding.forEach( (item)=>{
+    console.log( item.lname)
+})
